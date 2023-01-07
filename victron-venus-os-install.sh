52c7b3d4191b0e89381b99846179d08becd7167a
christian1980nrw <christian1980nrw@gmail.com>
2023-01-07 20:14:45 +0100

    Update victron-venus-os-install.sh

--- victron-venus-os-install.sh
+++ victron-venus-os-install.sh
@@ -3,7 +3,9 @@ mkdir /data/etc/Spotmarket-Switcher
 cd /data/etc/Spotmarket-Switcher
 wget https://raw.githubusercontent.com/christian1980nrw/Victron-ESS__AVM-Fritz-DECT200-210__Spotmarket-Switcher/main/data/etc/Spotmarket-Switcher/controller.sh
 chmod +x ./controller.sh
-(crontab -l | grep -Fxq "0 * * * * /data/etc/Spotmarket-Switcher/controller.sh") || (crontab -l; echo "0 * * * * /data/etc/Spotmarket-Switcher/controller.sh") | crontab -
+echo >> /data/rc.local
+echo "(crontab -l | grep -Fxq "0 * * * * /data/etc/Spotmarket-Switcher/controller.sh") || (crontab -l; echo "0 * * * * /data/etc/Spotmarket-Switcher/controller.sh") | crontab -" >> /data/rc.local
+chmod +x /data/rc.local
 
 echo Installation finished. Spotmarket-Switcher will be executed every full hour. 
 echo Please edit the file /data/etc/Spotmarket-Switcher/controller.sh and change it to your needs.
