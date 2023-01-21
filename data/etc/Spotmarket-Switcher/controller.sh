4e7a3292e8f188e82ff2c1430e278e7a10ef4047
christian1980nrw <christian1980nrw@gmail.com>
2023-01-21 01:22:30 +0100

    Update controller.sh

--- data/etc/Spotmarket-Switcher/controller.sh
+++ data/etc/Spotmarket-Switcher/controller.sh
@@ -15,6 +15,24 @@
 #Please note that this script is only for hourly based tariff data, please create your own fork if you need 15 minutes based data.
 #After every API reconfiguration please delete the old API-Downloadfiles with rm /tmp/awattar*.* /tmp/entsoe*.*
 
+# Switchable Sockets Setup (AVM Fritz DECT200/210 wireless sockets) if used (tested with FRITZ!OS: 07.29).
+use_fritz_dect_sockets=0 # please activate with 1 or deactivate this socket-type with 0
+fbox="192.168.178.1"
+user="fritz1234"
+passwd="YOURPASSWORD"
+sockets=("YOURSOCKETID" "0" "0" "0" "0" "0")
+
+# Switchable Sockets Setup (Shelly Wifi Plugs) (tested with Shelly Plug S Firmware 20230109-114426/v1.12.2-g32055ee)
+use_shelly_wlan_sockets=0  # please activate with 1 or deactivate this socket-type with 0
+shelly_ips=("192.168.178.89" "0" "0") # add multiple Shellys if you like, dont forget to make the ips static in your router
+shellyuser="admin"
+shellypasswd="YOURPASSWORD" # only if used
+
+# Solar Charger Setup (tested with Victron Venus OS)
+use_victron_charger=0 # please activate with 1 or deactivate this charger-type with 0
+charger_command_turnon="dbus -y com.victronenergy.settings /Settings/CGwacs/BatteryLife/Schedule/Charge/0/Day SetValue -- 7"
+charger_command_turnoff="dbus -y com.victronenergy.settings /Settings/CGwacs/BatteryLife/Schedule/Charge/0/Day SetValue -- -7"
+
 #Please change prices (always use Cent/kWh, no matter if youre using Awattar (displaying Cent/kWh) or Entsoe API (displaying EUR/MWh) / netto prices excl. tax).
 stop_price=4.1 # stop above this price
 start_price=2.0 # start below this price
@@ -23,21 +41,21 @@ energy_fee=15.3 # proofs of origin, allocations, duties and taxes (in case if st
 abort_price=50.1 # abort and never charge if actual price is same or higher than this (Energy fees not included)
 
 use_start_stop_logic=0 # Set to 1 to activate start/stop logic (start_stop_price).
-fritz_dect_at_start_stop=0 # You can add a additional load (like water heater) with AVM Fritz DECT200/210 switch sockets if you like.
+switchablesockets_at_start_stop=0 # You can add a additional load (like water heater) with AVM Fritz DECT200/210 switch sockets if you like.
 charge_at_solar_breakeven_logic=0 # Charge if energy including fees is cheaper than your own feedin-tariff of your solar system
-fritz_dect_at_solar_breakeven_logic=0
+switchablesockets_at_solar_breakeven_logic=0
 charge_at_lowest_price=0 # set 1 to charge at lowest price per day no matter which start/stop price was defined 
-fritz_dect_at_lowest_price=0
+switchablesockets_at_lowest_price=0
 charge_at_second_lowest_price=0
-fritz_dect_at_second_lowest_price=0
+switchablesockets_at_second_lowest_price=0
 charge_at_third_lowest_price=0
-fritz_dect_at_third_lowest_price=0
+switchablesockets_at_third_lowest_price=0
 charge_at_fourth_lowest_price=0
-fritz_dect_at_fourth_lowest_price=0
+switchablesockets_at_fourth_lowest_price=0
 charge_at_fifth_lowest_price=0
-fritz_dect_at_fifth_lowest_price=0
+switchablesockets_at_fifth_lowest_price=0
 charge_at_sixth_lowest_price=0
-fritz_dect_at_sixth_lowest_price=0
+switchablesockets_at_sixth_lowest_price=0
 TZ='Europe/Amsterdam' # Set Correct Timezone
 select_pricing_api=1 # please enter 1 for awattar or 2 for entsoe / Awattar: only germany DE-LU or austrian AT prices, but no API key needed / Entsoe: much more countrys available but free API key needed, see https://www.entsoe.eu/data/map/
 include_second_day=0 # Set to 0 to compare only the today prices. 
@@ -47,11 +65,14 @@ include_second_day=0 # Set to 0 to compare only the today prices.
 
 # Please set up Solar weather API to query solar yield
 use_solarweather_api_to_abort=0
-abort_solar_yield_today=3.0 # abort and never charge because we are expecting enough sun today (daily megajoule per squaremeter)
-abort_solar_yield_tomorrow=4.5 # abort and never charge because we are expecting enough sun tomorrow (daily megajoule per squaremeter)
+abort_solar_yield_today=4.5 # abort and never charge because we are expecting enough sun today (daily megajoule per squaremeter)
+abort_solar_yield_tomorrow=5.5 # abort and never charge because we are expecting enough sun tomorrow (daily megajoule per squaremeter)
 #To find the kilowatt hour value from megajoules, divide by 3.6. 
-country=Germany
-city=Dortmund
+abort_suntime=700 # abort and never charge if we have more sun minutes per day as this value (time in minutes between sunrise and sunset)
+latitude=51.530600 # Your location
+longitude=7.860575
+#You can use Google Maps to find the latitude and longitude of a location by searching for the address or location and then right-clicking
+#on the location on the map. A pop-up menu will appear with the option to "What's here?" which will display the latitude and longitude of that location.
 visualcrossing_api_key=YOURAPIKEY # Get your free key at https://www.visualcrossing.com/sign-up No credit card is required to sign up for your free 1000 records per day.
 
 # Awattar Api setup
@@ -71,12 +92,6 @@ entsoe_eu_api_security_token=YOURAPIKEY
 # After That you can generate a security token at https://transparency.entsoe.eu/usrm/user/myAccountSettings
 # The ENTSO-E Transparency Platform aims to provide free, continuous access to pan-European electricity market data for all users.
 
-# Switchable Sockets Setup (AVM Fritz DECT200/210 wireless sockets) if used (tested with FRITZ!OS: 07.29).
-fbox="192.168.178.1"
-user="fritz1234"
-passwd="YOURPASSWORD"
-sockets=("YOURSOCKETID1" "YOURSOCKETID2 or 0" "0" "0" "0" "0")
-
 # further Api parameters (no need to edit)
 yesterday=$(TZ=$TZ date -d @$(( $(TZ=$TZ date +"%s") - 86400)) +%d)2300
 yestermonth=$(TZ=$TZ date -d @$(( $(TZ=$TZ date +"%s") - 86400)) +%m)
@@ -93,7 +108,7 @@ getnow=$(TZ=$TZ date -d @$(( $(TZ=$TZ date +"%s") )) +%k)
 now_entsoe_linenumber=$(($getnow+1))
 link1=https://api.awattar.$awattar/v1/marketdata/current.yaml
 link2=http://api.awattar.$awattar/v1/marketdata/current.yaml?tomorrow=include
-link3="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$city%20$country/$todayyear-$todaymonth-$today2/$tomorrowyear-$tomorrowmonth-$tomorrow2?unitGroup=metric&elements=solarenergy&include=days&key=$visualcrossing_api_key&contentType=csv"
+link3="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/$latitude%2C%20$longitude/$todayyear-$todaymonth-$today2/$tomorrowyear-$tomorrowmonth-$tomorrow2?unitGroup=metric&elements=solarenergy%2Ccloudcover%2Csunrise%2Csunset&include=days&key=$visualcrossing_api_key&contentType=csv"
 link4="https://transparency.entsoe.eu/api?securityToken=$entsoe_eu_api_security_token&documentType=A44&in_Domain=$in_Domain&out_Domain=$out_Domain&periodStart=$yesteryear$yestermonth$yesterday&periodEnd=$todayyear$todaymonth$today"
 link5="https://transparency.entsoe.eu/api?securityToken=$entsoe_eu_api_security_token&documentType=A44&in_Domain=$in_Domain&out_Domain=$out_Domain&periodStart=$todayyear$todaymonth$today&periodEnd=$tomorrowyear$tomorrowmonth$tomorrow"
 file1=/tmp/awattar_today_prices.yaml
@@ -113,10 +128,12 @@ LOG_FILE="/data/etc/Spotmarket-Switcher/spotmarket-switcher.log"
 MAX_SIZE=1000 # 1 MB
 LOG_FILES_TO_KEEP=2
 
-
 ########## Begin of the script...
+
+if (( ( $use_victron_charger == 1 ) )); then
 echo "Maybe we are still charging from last script runtime. Stopping scheduled charging."
-dbus -y com.victronenergy.settings /Settings/CGwacs/BatteryLife/Schedule/Charge/0/Day SetValue -- -7
+$charger_command_turnoff
+fi
 
 download_awattar_prices() {
   local url=$1
@@ -240,8 +257,15 @@ do
 done
 }
 
-function get_solarenergy_today { solarenergy_today=$(sed -n 2{p} $file3 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | tail -n1); solarenergy_today_integer=$( printf "%.0f\n" "${solarenergy_today}e15" ); abort_solar_yield_today_integer=$( printf "%.0f\n" "${abort_solar_yield_today}e15" ); }
-function get_solarenergy_tomorrow { solarenergy_tomorrow=$(sed -n 3{p} $file3 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | tail -n1); solarenergy_tomorrow_integer=$( printf "%.0f\n" "${solarenergy_tomorrow}e15" ); abort_solar_yield_tomorrow_integer=$( printf "%.0f\n" "${abort_solar_yield_tomorrow}e15" );}
+function get_solarenergy_today { solarenergy_today=$(sed '2!d' $file3 | cut -d',' -f2); solarenergy_today_integer=$( printf "%.0f\n" "${solarenergy_today}e15" ); abort_solar_yield_today_integer=$( printf "%.0f\n" "${abort_solar_yield_today}e15" ); }
+function get_solarenergy_tomorrow { solarenergy_tomorrow=$(sed '3!d' $file3 | cut -d',' -f2); solarenergy_tomorrow_integer=$( printf "%.0f\n" "${solarenergy_tomorrow}e15" ); abort_solar_yield_tomorrow_integer=$( printf "%.0f\n" "${abort_solar_yield_tomorrow}e15" );}
+function get_cloudcover_today { cloudcover_today=$(sed '2!d' $file3 | cut -d',' -f1);}
+function get_cloudcover_tomorrow { cloudcover_tomorrow=$(sed '3!d' $file3 | cut -d',' -f1);}
+function get_sunrise_today { sunrise_today=$(sed '2!d' $file3 | cut -d',' -f3 | cut -d 'T' -f2 | awk -F: '{ print $1 ":" $2 }');}
+function get_sunset_today { sunset_today=$(sed '2!d' $file3 | cut -d',' -f4 | cut -d 'T' -f2 | awk -F: '{ print $1 ":" $2 }');}
+function get_suntime_today { suntime_today=$(((($(TZ=$TZ date -d "1970-01-01 $sunset_today" +%s) - $(TZ=$TZ date -d "1970-01-01 $sunrise_today" +%s))) / 60)); 
+}
+
 
 if (( ( $select_pricing_api == 1 ) )); then
 
@@ -335,9 +359,16 @@ if (( ( $use_solarweather_api_to_abort == 1 ) )); then
 download_solarenergy
 get_solarenergy_today
 get_solarenergy_tomorrow
+get_cloudcover_today
+get_cloudcover_tomorrow
+get_sunrise_today
+get_sunset_today
+get_suntime_today
+
 fi
 
 echo Please verify correct system time and timezone:
+echo >> $LOG_FILE
 TZ=$TZ date | tee -a $LOG_FILE
 echo "Current price is" $current_price" $Unit netto." | tee -a $LOG_FILE
 echo "Lowest price will be" $lowest_price" $Unit netto."
@@ -347,8 +378,10 @@ echo "Fourth lowest price will be" $fourth_lowest_price" $Unit netto."
 echo "Fifth lowest price will be" $fifth_lowest_price" $Unit netto."
 echo "Sixth lowest price will be" $sixth_lowest_price" $Unit netto."
 if (( ( $use_solarweather_api_to_abort == 1 ) )); then
-echo "Solarenergy today will be" $solarenergy_today" megajoule per sqaremeter."  | tee -a $LOG_FILE
-echo "Solarenergy tomorrow will be" $solarenergy_tomorrow" megajoule per squaremeter."  | tee -a $LOG_FILE
+echo "Sunrise today will be $sunrise_today and sunset will be $sunset_today. Suntime will be $suntime_today minutes. "  | tee -a $LOG_FILE
+echo "Solarenergy today will be" $solarenergy_today" megajoule per sqaremeter with "$cloudcover_today" percent clouds."  | tee -a $LOG_FILE
+echo "Solarenergy tomorrow will be" $solarenergy_tomorrow" megajoule per squaremeter with "$cloudcover_tomorrow" percent clouds."  | tee -a $LOG_FILE
+
 [ -s $file3 ]  && >nul || echo "Error: $file3 is empty, please check your API Key if download is still not possible tomorrow."
 find $file3 -size 0 -delete
 fi
@@ -362,8 +395,12 @@ if ((abort_price_integer <= current_price_integer)); then
   exit 0
 fi
 if ((use_solarweather_api_to_abort == 1)); then
+  if ((abort_suntime <= suntime_today)); then
+    echo "There are enough sun minutes today. Abort."  | tee -a $LOG_FILE
+    exit 0
+  fi
   if ((abort_solar_yield_today_integer <= solarenergy_today_integer)); then
-    echo "There is enough sun today. Abort."  | tee -a $LOG_FILE
+    echo "There is enough solarenergy today. Abort."  | tee -a $LOG_FILE
     exit 0
   fi
   if ((abort_solar_yield_tomorrow_integer <= solarenergy_tomorrow_integer)); then
@@ -383,7 +420,7 @@ charging_conditions=(
 )
 
 execute_charging=0
-execute_fritz_dect_on=0
+execute_switchablesockets_on=0
 
 # Check if any charging condition is met
 for condition in "${charging_conditions[@]}"; do
@@ -392,52 +429,49 @@ for condition in "${charging_conditions[@]}"; do
   break
   fi
 done
-
-fritz_dect_conditions=(
-"fritz_dect_at_start_stop == 1 && start_price_integer > current_price_integer"
-"fritz_dect_at_solar_breakeven_logic == 1  && feedin_price_integer > current_price_integer + energy_fee_integer"
-"fritz_dect_at_lowest_price == 1 && lowest_price_integer == current_price_integer"
-"fritz_dect_at_second_lowest_price == 1 && second_lowest_price_integer == current_price_integer"
-"fritz_dect_at_third_lowest_price == 1 && third_lowest_price_integer == current_price_integer"
-"fritz_dect_at_fourth_lowest_price == 1  && fourth_lowest_price_integer == current_price_integer"
-"fritz_dect_at_fifth_lowest_price == 1 && fifth_lowest_price_integer == current_price_integer"
-"fritz_dect_at_sixth_lowest_price == 1 && sixth_lowest_price_integer == current_price_integer"
+switchablesockets_conditions=(
+"switchablesockets_at_start_stop == 1 && start_price_integer > current_price_integer"
+"switchablesockets_at_solar_breakeven_logic == 1  && feedin_price_integer > current_price_integer + energy_fee_integer"
+"switchablesockets_at_lowest_price == 1 && lowest_price_integer == current_price_integer"
+"switchablesockets_at_second_lowest_price == 1 && second_lowest_price_integer == current_price_integer"
+"switchablesockets_at_third_lowest_price == 1 && third_lowest_price_integer == current_price_integer"
+"switchablesockets_at_fourth_lowest_price == 1  && fourth_lowest_price_integer == current_price_integer"
+"switchablesockets_at_fifth_lowest_price == 1 && fifth_lowest_price_integer == current_price_integer"
+"switchablesockets_at_sixth_lowest_price == 1 && sixth_lowest_price_integer == current_price_integer"
 )
 
-# Check if any Fritz DECT condition is met
-for fritz_dect_condition in "${fritz_dect_conditions[@]}"; do
-  if (( $fritz_dect_condition == 1 )); then
-    execute_fritz_dect_on=1
+# Check if any switching condition is met
+for switchablesockets_condition in "${switchablesockets_conditions[@]}"; do
+  if (( $switchablesockets_condition == 1 )); then
+    execute_switchablesockets_on=1
   break
   fi
 done
-
 # If any charging condition is met, start charging
-if (( execute_charging == 1 )); then
+if (( execute_charging == 1 && $use_victron_charger == 1 )); then
 echo " Executing 1 hour charging." | tee -a $LOG_FILE
-  dbus -y com.victronenergy.settings /Settings/CGwacs/BatteryLife/Schedule/Charge/0/Day SetValue -- 7
+  $charger_command_turnon
 fi
 
-# If any Fritz DECT condition is met, execute Fritz DECT on command
-  if (( execute_fritz_dect_on == 1 )); then
-echo " Executing 1 hour switching." | tee -a $LOG_FILE
-
+# execute Fritz DECT on command
+  if (( execute_switchablesockets_on == 1 && use_fritz_dect_sockets == 1 )); then
+echo " Executing 1 hour Fritz switching." | tee -a $LOG_FILE
 # Get session ID (SID)
 sid=""
 challenge=$(curl -s "http://$fbox/login_sid.lua" | grep -o "<Challenge>[a-z0-9]\{8\}" | cut -d'>' -f 2)
-if [ -z "$challenge" ]; then
+	if [ -z "$challenge" ]; then
     printf "Error: Could not retrieve challenge from login_sid.lua.\n"
     exit 1
-fi
+	fi
 
 hash=$(echo -n "$challenge-$passwd" |sed -e 's,.,&\n,g' | tr '\n' '\0' | md5sum | grep -o "[0-9a-z]\{32\}")
 sid=$(curl -s "http://$fbox/login_sid.lua" -d "response=$challenge-$hash" -d "username=$user" \
     | grep -o "<SID>[a-z0-9]\{16\}" |  cut -d'>' -f 2)
-if [ "$sid" = "0000000000000000" ]; then
+	if [ "$sid" = "0000000000000000" ]; then
     printf "Error: Login to Fritzbox failed.\n" | tee -a $LOG_FILE
     exit 1
-fi
-printf "Login to Fritzbox successful.\n\n" | tee -a $LOG_FILE
+	fi
+printf "Login to Fritzbox successful.\n" | tee -a $LOG_FILE
 # Iterate over each socket
 for socket in "${sockets[@]}"; do
     if [ "$socket" = "0" ]; then
@@ -455,10 +489,35 @@ for socket in "${sockets[@]}"; do
         printf "Socket $socket is not connected\n" | tee -a $LOG_FILE
     fi
 done
+fi
 
+  if (( execute_switchablesockets_on == 1 && use_shelly_wlan_sockets == 1 )); then
+  
+  for ip in "${shelly_ips[@]}"
+do
+  if [ $ip != "0" ]; then
+    echo " Executing 1 hour Shelly switching." | tee -a $LOG_FILE
+    curl -u '$shellyuser:$shellypasswd' http://$ip/relay/0?turn=on
+  fi
+done
+
+  fi
+if [ $execute_charging -eq 1 ] || [ $execute_switchablesockets_on -eq 1 ]; then
 # Wait for almost 60 minutes
 sleep 3560
+fi
 
+  if (( execute_switchablesockets_on == 1 && use_shelly_wlan_sockets == 1 )); then
+  
+  for ip in "${shelly_ips[@]}"
+do
+  if [ $ip != "0" ]; then
+    curl -u '$shellyuser:$shellypasswd' http://$ip/relay/0?turn=off
+  fi
+done
+
+  fi
+  if (( execute_switchablesockets_on == 1 && use_fritz_dect_sockets == 1 )); then
 # Turn off each socket
 for socket in "${sockets[@]}"; do
     if [ "$socket" = "0" ]; then
@@ -469,7 +528,6 @@ done
 fi
 
 # doing logrotation
-echo >> $LOG_FILE
 if [ -f "$LOG_FILE" ]; then
   if [ $(du -k "$LOG_FILE" | awk '{print $1}') -gt "$MAX_SIZE" ]; then
     mv "$LOG_FILE" "$LOG_FILE".$(date +%Y%m%d%H%M%S)
