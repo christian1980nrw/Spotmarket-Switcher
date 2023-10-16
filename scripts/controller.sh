ee0d5310fc8d1e693c0e128d7f1dd7e7d3f2192a
Christian Kvasny <chris@ckvsoft.at>
2023-10-16 19:41:42 +0200

    Update controller.sh
    add check for config file

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -131,6 +131,16 @@ DIR="$(dirname "$0")"
 # Include the configuration file
 source "$DIR/config.txt"
 
+FILE_TO_INCLUDE="/pfad/zur/datei.sh"
+
+if [ -f "$DIR/config.txt" ]; then
+    source "$DIR/config.txt"
+else
+    echo "The file $DIR/config.txt was not found! Configure the existing sample.config.txt file and then save it as config.txt in the same directory."
+    exit 1
+fi
+
+
 get_tibber_api() {
 	curl --location --request POST 'https://api.tibber.com/v1-beta/gql' \
 		--header 'Content-Type: application/json' \
