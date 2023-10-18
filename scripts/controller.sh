1fdd1c397d52024ed4e3570d516b2f5bb09b1c54
Christian Kvasny <chris@ckvsoft.at>
2023-10-18 16:16:52 +0200

    Update controller.sh
    Fix

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -556,16 +556,13 @@ is_charging_economical() {
     local reference_price="$1"
     local total_cost=$((current_price_integer + percent_of_current_price_integer + battery_lifecycle_costs_cent_per_kwh_integer))
 
-    # Evaluate the condition and store in a variable
     local is_economical=0
     [[ $reference_price -ge $total_cost ]] && is_economical=1
 
-    # Send debug output to stderr
     if [ -n "$DEBUG" ]; then
         echo "D: is_charging_economical [ $is_economical - $( [ "$is_economical" -eq 1 ] && echo "true" || echo "false" ) ]." >&2
     fi
 
-    # Return the result
     return $is_economical
 }
 
@@ -875,9 +872,9 @@ percent_of_current_price_integer=$(awk "BEGIN {print $current_price_integer*$ene
 if ((execute_charging == 1 && use_victron_charger == 1)); then
 	if ((economic_check == 0)); then
 		manage_charging "on" "Charging based on condition met of: $charging_condition_met."
-	elif ((economic_check == 1 && $(is_charging_economical $highest_price_integer))); then
+	elif ((economic_check == 1 && is_charging_economical $highest_price_integer)); then
 		manage_charging "on" "Charging based on highest price comparison makes sense."
-	elif ((economic_check == 2 && $(is_charging_economical $average_price_integer))); then
+	elif ((economic_check == 2 && is_charging_economical $average_price_integer)); then
 		manage_charging "on" "Charging based on average price comparison makes sense."
 	else
 		manage_charging "off" "Considering charging losses and costs, charging is too expensive."
