818b8837158a4e4431040efa75951a66d3eceff4
Christian Butterweck <christian1980nrw@gmail.com>
2023-10-20 21:13:08 +0200

    Update victron-venus-os-install.sh

--- victron-venus-os-install.sh
+++ victron-venus-os-install.sh
@@ -98,16 +98,16 @@ fi
 #
 #Set Colors (http://natelandau.com/bash-scripting-utilities/)
 #
+#FIXME: see https://github.com/christian1980nrw/Spotmarket-Switcher/pull/105
+#bold=$(tput bold)
+#underline=$(tput sgr 0 1)
+#reset=$(tput sgr0)
 
-bold=$(tput bold)
-underline=$(tput sgr 0 1)
-reset=$(tput sgr0)
-
-purple=$(tput setaf 171)
-red=$(tput setaf 1)
-green=$(tput setaf 76)
-tan=$(tput setaf 3)
-blue=$(tput setaf 38)
+#purple=$(tput setaf 171)
+#red=$(tput setaf 1)
+#green=$(tput setaf 76)
+#tan=$(tput setaf 3)
+#blue=$(tput setaf 38)
 
 #
 # Headers and  Logging
