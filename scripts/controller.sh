5e1321181334c833719a40a25d9ace9e03130152
Christian Kvasny <chris@ckvsoft.at>
2023-12-02 13:08:43 +0100

    Update controller.sh
    Fix Fritz/Shelly when disablet
    This fix got lost somewhere. I had already done that

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -636,6 +636,11 @@ check_abort_condition() {
 # Function to manage fritz sockets and log a message
 manage_fritz_sockets() {
     local action=$1
+    
+    if [ "$execute_switchablesockets_on" -eq 0 ]; then
+        log_message "I: execute_switchablesockets_on is 0. Exiting manage_fritz_sockets."
+        return
+    fi
 
     [ "$action" != "off" ] && action=$([ "$execute_switchablesockets_on" == "1" ] && echo "on" || echo "off")
 
@@ -688,6 +693,11 @@ fritz_login() {
 # Function to manage shelly and log a message
 manage_shelly_sockets() {
     local action=$1
+
+    if [ "$execute_switchablesockets_on" -eq 0 ]; then
+        log_message "I: execute_switchablesockets_on is 0. Exiting manage_shelly_sockets."
+        return
+    fi
 
     [ "$action" != "off" ] && action=$([ "$execute_switchablesockets_on" == "1" ] && echo "on" || echo "off")
 
